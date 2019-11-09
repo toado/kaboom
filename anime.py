fa78da57a9118db1ed5352ced2be05d774b31718
MAY <schen7652@gmail.com>
2019-11-09 16:11:44 -0700

    Implemented Web Scraping of the User Entered Show

--- anime.py
+++ anime.py
@@ -3,13 +3,53 @@ from urllib.request import urlopen
 from googlesearch import search
 
 
+class Show:
+    def __init__(self, url):
+        self.url = url
+        self.soup = BeautifulSoup(openUrl(url), "html.parser")
+        self.title = ""
+        self.image = ""
+        self.score = ""
+        self.year = ""
+        self.genres = []
+
+    def getInfo(self):
+        soup = self.soup
+
+        title = soup.findAll("span", {"itemprop":"name"})
+        self.title = title[0].text.strip()
+
+        image = soup.findAll("img", {"class":"ac"})
+        self.image = image[0]["src"].strip()
+
+        score = soup.findAll("div", {"class":"fl-l score"})
+        self.score = score[0].text.strip()
+
+        year = soup.findAll("span", text="Premiered:")
+        self.year = year[0].parent.a.text.split()[1]
+
+        genresHTML = soup.findAll("span", text="Genres:")
+        genresHTML = genresHTML[0].parent.findAll("a")
+        for genreHTML in genresHTML:
+            self.genres.append(genreHTML.text.strip())
+
+    def printInfo(self):
+        print("Title: " + self.title)
+        print("Image: " + self.image)
+        print("Score: " + self.score)
+        print("Year: " + self.year)
+        print("Genres: ", end="")
+        for genre in self.genres:
+            print(genre, end=" ")
+
+
 def main():
     query = input("Enter the title: ")
     url = getUrl(query)
-    pageHTML = openUrl(url)
 
-    soup = BeautifulSoup(pageHTML, "html.parser")
-    getInfo(soup)
+    userShow = Show(url)
+    userShow.getInfo()
+    userShow.printInfo()
 
 
 def getUrl(query):
@@ -24,10 +64,6 @@ def openUrl(url):
     pageHTML = client.read()
     client.close()
     return pageHTML
-
-
-def getInfo(soup):
-    pass
 
 
 if __name__ == "__main__":
