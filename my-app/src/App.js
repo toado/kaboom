35a69ef12f6ae5ede7b3319729e5d2d82a59f14d
Scott <skchu@ualberta.ca>
2019-11-09 16:21:17 -0700

    react files

---
+++ my-app/src/App.js
@@ -0,0 +1,11 @@
+import React, { component } from 'react';
+import Test from './components/Search'
+import './App.css';
+
+function App() {
+  return (
+    <h1>CD Movies</h1>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/Search.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const  = React.createClass({
+  render () {
+    return (
+
+    )
+  }
+})
+
+export default
