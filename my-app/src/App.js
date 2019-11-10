b8dfe9506646ecc9c016e7767095d4427cc8df6c
Scott <skchu@ualberta.ca>
2019-11-09 23:15:57 -0700

    adding components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,20 @@
-import React, { component } from 'react';
-import Test from './components/Search'
+import React from 'react';
+import Title from './components/Title'
+import Search from './components/Search'
+import Select from './components/Select'
 import './App.css';
 
-function App() {
-  return (
-    <h1>CD Movies</h1>
-  );
-}
+export default class landingPage extends React.Component {
+  render() {
+    return(
+      <div className = "main">
+        <Title />
+        <Search />
+        <br>
+        </br>
+        <Select />
 
-export default App;
+      </div>
+    )
+  }
+}
--- my-app/src/components/Search.js
+++ my-app/src/components/Search.js
@@ -1,12 +1,38 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from 'react';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
+import '../App.css';
 
-const  = React.createClass({
-  render () {
-    return (
 
-    )
-  }
-})
+export default class Search extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             filmName: ""
+        }
+    }
 
-export default
+    filmNameChange = (event) => {
+        console.log(event.target.value)
+        this.setState({
+            filmName: event.target.value 
+        })
+
+    }
+    
+    render() {
+        return (
+            <div className = "container-fluid">
+                <div className = "row align-items-center justify-content-center">
+                    <div>
+                    <Form inline className = "searchBox" value = {this.state.filmName} onChange = {this.filmNameChange}>
+                        <FormControl type="text" placeholder="Search" />
+                    </Form>
+                    </div>
+                </div>
+            </div>
+
+        )
+    }
+}
